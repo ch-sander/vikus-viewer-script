9129a1e4fc930448c401e5eb33f6be46835d6b64
ch-sander <74921235+ch-sander@users.noreply.github.com>
2021-08-07 18:56:26 +0200

    Delete command.bat

--- command.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\Sander\Documents\GitHub\vikus-viewer-script
-vikus-viewer-script "C:\Users\Sander\Pictures\Peregrinus\items2\*.jpg" -- output C:\Users\Sander\Pictures\Peregrinus\output
-pause
