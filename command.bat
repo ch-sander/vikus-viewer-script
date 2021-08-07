134b545d6a7de21b6f01782eb051dfc21cbf2a5b
ch-sander <74921235+ch-sander@users.noreply.github.com>
2021-08-07 18:40:15 +0200

    vikus

---
+++ command.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Sander\Documents\GitHub\vikus-viewer-script
+vikus-viewer-script "C:\Users\Sander\Pictures\Peregrinus\items2\*.jpg" -- output C:\Users\Sander\Pictures\Peregrinus\output
+pause
